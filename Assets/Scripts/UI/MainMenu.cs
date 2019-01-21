1ddce9aff4d56613c7eb974011f529000802d912
Adrien Allard <adrien.allard.pro@gmail.com>
2019-01-21 22:27:09 +0100

    Add main menu and bootstraper

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+    }
+
+    public void JoinGame()
+    {
+    }
+
+    public void LoadGame()
+    {
+    }
+}
