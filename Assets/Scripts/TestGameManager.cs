6b8ed833e55a6b37b308767de2ba757ba4f9e1e8
Adrien Allard <adrien.allard.pro@gmail.com>
2019-02-04 22:10:07 +0100

    Add test scene

---
+++ Assets/Scripts/TestGameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Simulation;
+using Simulation.Network;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestGameManager : MonoBehaviour
+{
+    private GameServer server;
+    private GameClient client;
+
+    [SerializeField]
+    private uint durationBetweenTwoTicks = 1000;
+    [SerializeField]
+    private ulong timeElapsedPerTick = 1;
+
+    private async void Start()
+    {
+        this.server = new GameServer(this.durationBetweenTwoTicks);
+        this.server.Start();
+
+        this.client = new GameClient();
+        this.client.Start();
+
+        var gameInstanceId = await this.client.PostCreateGameOrder(this.timeElapsedPerTick);
+        await this.client.PostJoinGameOrder(gameInstanceId);
+
+        SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        this.client?.Update();
+
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //this.Players[0].Resources[(int)ResourceType.AssemblingMachine1].Amount += new Number(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            //this.Players[0].Resources[(int)ResourceType.SciencePack1].Amount += new Number(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            //this.TimeElapsedPerTick *= 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            //if (this.TimeElapsedPerTick > 1)
+            //{
+            //    this.TimeElapsedPerTick /= 2;
+            //}
+        }
+#endif
+    }
+
+    private void OnApplicationQuit()
+    {
+        this.client?.Stop();
+        this.server?.Stop();
+    }
+}
